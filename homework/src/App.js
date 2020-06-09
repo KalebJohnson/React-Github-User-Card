ebd7437cc963ddb4abf51b262958bc04a35c3f8e
unknown <KalebJohnson45@yahoo.com>
2020-06-09 18:16:13 -0500

    Done

---
+++ homework/src/App.js
@@ -0,0 +1,31 @@
+
+import React, { Component } from 'react';
+import axios from 'axios';
+import User from './components/User'
+
+class App extends Component {
+  state = {user: [], followers: []};
+
+  componentDidMount() {
+
+    axios
+      .get('https://api.github.com/users/KalebJohnson')
+      .then(res => this.setState({ user: res.data }))
+      .catch(err => console.log(err));
+
+    axios
+      .get(`https://api.github.com/users/KalebJohnson/followers`)
+      .then(res => this.setState({ followers: res.data }))
+      .catch(err => console.log(err))
+
+  }
+
+  render() {
+    return (<>
+      <h1>GitHub User</h1>
+      <User user={this.state.user} followers={this.state.followers} />
+    </>)
+  }
+}
+
+export default App;
---
+++ homework/src/components/Followers.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Followers = props => {
+  return (<>
+    <h2>{props.follower.login}</h2>
+    <img src={props.follower.avatar_url} alt='' />
+  </>);
+};
+export default Followers;
